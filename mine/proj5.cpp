66834656aad43d7e423dcd18470e297ee7e049d6
Christian Delpin <chrisakde@gmail.com>
2024-11-19 19:43:55 -0500

    stuff

--- mine/proj5.cpp
+++ mine/proj5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "hashtable.h"
+#include "passserver.h"
 
 using namespace std;
 using namespace cop4530;
@@ -8,6 +9,8 @@ void Menu();
 
 //        usrnme  passwd
 HashTable<string, string> table;
+PassServer ps;
+
 
 int main()
 {
@@ -20,20 +23,53 @@ int main()
     switch(choice)
     {
       case 'l':
+        string filename;
+        cout << "Enter filename: ";
+        cin >> filename;
+        if(!ps.load(filename))
+        {
+          cerr << "Error loading file.";
+          //if necessary to output more specific errors, add here?
+        }
+        else
+        {
+          cout << "Loaded " << filename << " successfully." << endl;
+          //maybe do something after loaded?
+        }
         break;
       case 'a':
+        ps.addUser(/**/);
         break;
       case 'r':
+        ps.removeUser(/**/)
         break;
       case 'c':
+        ps.changePassword(/**/)
         break;
       case 'f':
+        string username;
+        cout << "Enter username to find: ";
+        cin >> username;
+        if(ps.find(username))
+        {
+          cout << username << " found." << endl;
+        }
+        else
+        {
+          cout << username << " not found." << endl;
+        }
         break;
       case 'd':
+        ps.dump();
+        break;
+      case 's':
+        ps.size();
         break;
       case 'w':
+        ps.write_to_file(/*filename to create*/);
         break;
       case 'x':
+        //nothing goes here.
         break;
       default:
         cout << "Invalid input. " << endl;
